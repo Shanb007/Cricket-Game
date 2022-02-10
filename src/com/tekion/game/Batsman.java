b1e78143d4c9606deaf121672368657009aaeda9
shanb7 <shantanubhallakkb@gmail.com>
2022-02-11 01:45:03 +0530

    added 2 classes and deleted one, added methods to other classes as well.

---
+++ src/com/tekion/game/Batsman.java
@@ -0,0 +1,13 @@
+package com.tekion.game;
+
+public class Batsman {
+    String BatsmanName;
+    Double StrikeRate;
+    int NoOf50s;
+    int NoOf100s;
+    String BatsmanHanded;
+    int TotalMatchesPlayed;
+    int TotalRunsSoFar;
+
+    //methods
+}
---
+++ src/com/tekion/game/Bowler.java
@@ -0,0 +1,15 @@
+package com.tekion.game;
+
+public class Bowler {
+String BowlerName;
+double Econ;
+int noOf5w;
+int noOf10w;
+int TotalWicketsTakenSoFar;
+int TotalMatchesPlayed;
+String BowlerHanded;
+String BowlerType;
+
+//methods
+
+}
